a832051bebbf7f33709bbd9839824b755164b026
kimi117 <shenqilzb@163.com>
2021-09-04 10:11:13 +0800

    init Repository

---
+++ src/main/java/com/example/life_community/LifeCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.life_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/life_community/LifeCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.life_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
