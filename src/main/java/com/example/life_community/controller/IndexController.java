d1d0424e722573b3693b2a83d28143c6a5a16a83
kimi117 <shenqilzb@163.com>
2021-09-05 09:23:37 +0800

    index.html

---
+++ src/main/java/com/example/life_community/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.example.life_community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        System.out.println("Index Controller");
+        return "index";
+    }
+
+}
